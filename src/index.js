356cb77cfeaef099b6e0f266aa5c47c4e412dc54
Kaua-Sun <kaua.lua717@gmail.com>
2026-05-14 16:25:01 -0300

    desafio concluído

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console .log("Olá Mundo")
+var nomeDoJogador = "Kauã"
+var nivelDeXpAtual = 2543
+
+if (nivelDeXpAtual <= 1000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Ferro")
+}
+else if (nivelDeXpAtual >= 1001 && nivelDeXpAtual <=2000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Bronze")
+}
+else if (nivelDeXpAtual >= 2001 && nivelDeXpAtual <=5000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Prata")
+}
+else if (nivelDeXpAtual >= 5001 && nivelDeXpAtual <=7000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Ouro")
+}
+else if (nivelDeXpAtual >= 7001 && nivelDeXpAtual <=8000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Platina")
+}
+else if (nivelDeXpAtual >= 8001 && nivelDeXpAtual <=9000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Ascendente")
+}
+else if (nivelDeXpAtual >= 9001 && nivelDeXpAtual <=10000) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Imortal")
+}
+else if (nivelDeXpAtual >= 10001) {
+    console.log("O Herói de nome " + nomeDoJogador + " está no nível de Radiante")
+}
